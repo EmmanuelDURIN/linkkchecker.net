3901bc120a2b1727be14f80d4e5fcff5b8eadefd
Emmanuel DURIN <emmanuel.durin@gmail.com>
2019-08-25 18:28:24 +0200

    Upgradeable Locks

--- SpiderInterface/ScanResults.cs
+++ SpiderInterface/ScanResults.cs
@@ -6,93 +6,101 @@ using System.Threading;
 
 namespace SpiderInterface
 {
-  public class ScanResults : IEnumerable<KeyValuePair<Uri, ScanResult>>
-  {
+    public class ScanResults : IEnumerable<KeyValuePair<Uri, ScanResult>>
+    {
 
-    private ReaderWriterLockSlim slimLock = new ReaderWriterLockSlim(LockRecursionPolicy.SupportsRecursion);
+        private ReaderWriterLockSlim scanResultsLock = new ReaderWriterLockSlim(LockRecursionPolicy.SupportsRecursion);
 
-    private Dictionary<Uri, ScanResult> results { get; set; } = new Dictionary<Uri, ScanResult>();
-    public int FailureCount { get => results.Count(sr => (sr.Value.Status != null && sr.Value.Status.Value.IsSuccess()) || sr.Value.Exception != null); }
-    public bool TryGetScanResult(Uri uri, out ScanResult scanResult)
-    {
-      bool result;
-      slimLock.EnterWriteLock();
-      try
-      {
-        if (!(result = results.ContainsKey(uri)))
+        private Dictionary<Uri, ScanResult> results { get; set; } = new Dictionary<Uri, ScanResult>();
+        public int FailureCount { get => results.Count(sr => (sr.Value.Status != null && sr.Value.Status.Value.IsSuccess()) || sr.Value.Exception != null); }
+        public bool TryGetScanResult(Uri uri, out ScanResult scanResult)
         {
-          scanResult = new ScanResult();
-          results.Add(uri, scanResult);
+            bool result;
+            scanResultsLock.EnterUpgradeableReadLock();
+            try
+            {
+                if (!(result = results.ContainsKey(uri)))
+                {
+                    scanResult = new ScanResult();
+                    try
+                    {
+                        scanResultsLock.EnterWriteLock();
+                        results.Add(uri, scanResult);
+                    }
+                    finally
+                    {
+                        scanResultsLock.ExitWriteLock();
+                    }
+                }
+                else
+                {
+                    scanResult = results[uri];
+                }
+            }
+            finally
+            {
+                scanResultsLock.ExitUpgradeableReadLock();
+            }
+            return result;
         }
-        else
-        {
-          scanResult = results[uri];
-        }
-      }
-      finally
-      {
-        slimLock.ExitWriteLock();
-      }
-      return result;
-    }
 
-    public void Replace(Uri uri, ScanResult scanResult)
-    {
-      slimLock.EnterWriteLock();
-      try
-      {
-        if (results.ContainsKey(uri))
+        public void Replace(Uri uri, ScanResult scanResult)
         {
-          results.Remove(uri);
+            scanResultsLock.EnterWriteLock();
+            try
+            {
+                if (results.ContainsKey(uri))
+                {
+                    results.Remove(uri);
+                }
+                results.Add(uri, scanResult);
+            }
+            finally
+            {
+                scanResultsLock.ExitWriteLock();
+            }
         }
-        results.Add(uri, scanResult);
-      }
-      finally
-      {
-        slimLock.ExitWriteLock();
-      }
-    }
-    public void AddOrReplace(Uri uri, ScanResult scanResult)
-    {
-      slimLock.EnterWriteLock();
-      try
-      {
-        if (results.ContainsKey(uri))
+        public void AddOrReplace(Uri uri, ScanResult scanResult)
         {
-          results[uri] = scanResult;
+            scanResultsLock.EnterWriteLock();
+            try
+            {
+                if (results.ContainsKey(uri))
+                {
+                    results[uri] = scanResult;
+                }
+                else
+                {
+                    results.Add(uri, scanResult);
+                }
+            }
+            finally
+            {
+                scanResultsLock.ExitWriteLock();
+            }
         }
-        else
-        { 
-          results.Add(uri, scanResult);
+        public bool ContainsKey(Uri uri)
+        {
+            scanResultsLock.EnterReadLock();
+            try
+            {
+                return results.ContainsKey(uri);
+            }
+            finally
+            {
+                scanResultsLock.ExitReadLock();
+            }
         }
-      }
-      finally
-      {
-        slimLock.ExitWriteLock();
-      }
+        public IEnumerator<KeyValuePair<Uri, ScanResult>> GetEnumerator()
+        {
+            foreach (var sr in results)
+            {
+                yield return sr;
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<KeyValuePair<Uri, ScanResult>>)this).GetEnumerator();
+        }
     }
-    public bool ContainsKey(Uri uri)
-    {
-      slimLock.EnterReadLock();
-      try
-      {
-        return results.ContainsKey(uri);
-      }
-      finally
-      {
-        slimLock.ExitReadLock();
-      }
-    }
-    public IEnumerator<KeyValuePair<Uri, ScanResult>> GetEnumerator()
-    {
-      foreach (var sr in results)
-      {
-        yield return sr;
-      }
-    }
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-      return ((IEnumerable<KeyValuePair<Uri, ScanResult>>)this).GetEnumerator();
-    }
-  }
 }
