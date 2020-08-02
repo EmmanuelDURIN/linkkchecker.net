3e8ce4f09cff13407dcdb8fafea055dc98df2f26
Emmanuel Durin <emmanueldurin@gmail.com>
2020-08-02 11:18:10 +0200

    Use ConcurrentExclusiveSchedulerPair

--- LinkChecker/SingleThreadedLogger.cs
+++ LinkChecker/SingleThreadedLogger.cs
@@ -1,44 +1,66 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 using SpiderInterface;
 
 namespace LinkChecker
 {
     internal class SingleThreadedLogger
     {
-        private static object logLock = new object();
+        private static ConcurrentExclusiveSchedulerPair schedulerPair = new ConcurrentExclusiveSchedulerPair(TaskScheduler.Default, maxConcurrencyLevel: 1);
+        private static TaskScheduler exclusiveScheduler = schedulerPair.ExclusiveScheduler;
+
+        static SingleThreadedLogger()
+        {
+            exclusiveScheduler = schedulerPair.ExclusiveScheduler;
+        }
         internal static void LogException(Exception ex, Uri parentUri, Uri uri)
         {
-            lock (logLock)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"Exception {ex.Message} processing {uri} parent is {parentUri}");
-                Console.ForegroundColor = ConsoleColor.White;
-            }
+            Task.Factory.StartNew
+                (
+                        () =>
+                        {
+                            Console.WriteLine($"Execution of log on thread {Thread.CurrentThread.ManagedThreadId}");
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine($"Exception {ex.Message} processing {uri} parent is {parentUri}");
+                            Console.ForegroundColor = ConsoleColor.White;
+                        },
+                        CancellationToken.None,
+                        TaskCreationOptions.None,
+                        exclusiveScheduler
+                );
         }
         internal static void Log(string msg, MessageSeverity severity)
         {
-            lock (logLock)
-            {
-                switch (severity)
+            Task.Factory.StartNew
+            (
+                () =>
                 {
-                    case MessageSeverity.Success:
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        break;
-                    case MessageSeverity.Info:
-                        Console.ForegroundColor = ConsoleColor.White;
-                        break;
-                    case MessageSeverity.Warn:
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                        break;
-                    case MessageSeverity.Error:
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        break;
-                    default:
-                        break;
-                }
-                Console.WriteLine(msg);
-                Console.ForegroundColor = ConsoleColor.White;
-            }
+                    Console.WriteLine($"Execution of log on thread {Thread.CurrentThread.ManagedThreadId}");
+                    switch (severity)
+                    {
+                        case MessageSeverity.Success:
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            break;
+                        case MessageSeverity.Info:
+                            Console.ForegroundColor = ConsoleColor.White;
+                            break;
+                        case MessageSeverity.Warn:
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            break;
+                        case MessageSeverity.Error:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            break;
+                        default:
+                            break;
+                    }
+                    Console.WriteLine(msg);
+                    Console.ForegroundColor = ConsoleColor.White;
+                },
+                CancellationToken.None,
+                TaskCreationOptions.None,
+                exclusiveScheduler
+            );
         }
     }
 }
