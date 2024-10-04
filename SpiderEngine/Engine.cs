d4ea033b19966303eff59f83229d32a217a324d8
Emmanuel Durin <emmanueldurin@gmail.com>
2024-10-04 08:03:24 +0200

    Remove Task.Run and add await

--- SpiderEngine/Engine.cs
+++ SpiderEngine/Engine.cs
@@ -47,7 +47,7 @@ namespace SpiderEngine
             {
                 LogException(ex, null, BaseUri);
             }
-            Done();
+            await DoneAsync();
         }
         private void Init()
         {
@@ -58,11 +58,11 @@ namespace SpiderEngine
                 extension.Init();
             }
         }
-        private void Done()
+        private async Task DoneAsync()
         {
             foreach (var extension in Extensions)
             {
-                extension.Done();
+                await extension.DoneAsync();
             }
             stopwatch.Stop();
             Logger?.Invoke($"Finished crawling at {DateTime.Now}", MessageSeverity.Success);
@@ -224,15 +224,8 @@ namespace SpiderEngine
                 string attributeName = pair.Value;
                 IEnumerable<HtmlNode> links = documentNode.Descendants(tagName);
                 List<Task> tasks = links
-                    .Select(link =>  Task.Run( async() => await ScanLinkAsync(steps, uri, attributeName, link) ) )
+                    .Select(link => ScanLinkAsync(steps, uri, attributeName, link) )
                     .ToList();
-                // Equivalent à 
-                //List<Task> tasks = new List<Task>();
-                //foreach (var link in links)
-                //{
-                //    Task t = Task.Run(async () => { await ScanLinkAsync(steps, uri, attributeName, link); });
-                //    tasks.Add(t);
-                //}
                 await Task.WhenAll(tasks);
             }
         }
--- SpiderInterface/ISpiderExtension.cs
+++ SpiderInterface/ISpiderExtension.cs
@@ -7,6 +7,6 @@ namespace SpiderInterface
         IEngine? Engine { get; set; }
         void Init();
         void Process(List<CrawlStep> steps, Uri uri, HttpResponseMessage responseMessage, HtmlDocument? doc);
-        Task Done();
+        Task DoneAsync();
     }
 }
