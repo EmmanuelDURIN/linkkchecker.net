e451a4f82f882255f7f39e24b442d3862e29b5eb
Emmanuel Durin <emmanueldurin@gmail.com>
2025-10-08 20:39:17 +0200

    .Net9

--- SpiderEngine/Engine.cs
+++ SpiderEngine/Engine.cs
@@ -4,7 +4,6 @@ using System.Collections.Immutable;
 using System.Diagnostics;
 using System.Net;
 using System.Text;
-using System.Threading;
 
 namespace SpiderEngine
 {
@@ -34,7 +33,7 @@ namespace SpiderEngine
                 }
             }
         }
-        private Stopwatch stopwatch = new Stopwatch();
+        private readonly Stopwatch stopwatch = new Stopwatch();
         public async Task StartAsync(CancellationToken cancellationToken)
         {
             Uri? startUri = Config?.StartUri;
@@ -47,14 +46,14 @@ namespace SpiderEngine
                               parentUri: null,
                               uri: startUri,
                               pageMayContainsLink: true,
-                              processChildrenLinks:true,
+                              processChildrenLinks: true,
                               cancellationToken: cancellationToken);
             }
             catch (Exception ex)
             {
                 LogException(ex, null, BaseUri);
             }
-             await DoneAsync(cancellationToken);
+            await DoneAsync(cancellationToken);
         }
         private void Init()
         {
@@ -156,7 +155,7 @@ namespace SpiderEngine
                     case (int)HttpStatusCode.RedirectKeepVerb:
                     case (int)HttpStatusCode.SeeOther:
                         // TODO process redirections
-                        Debug.Assert(false, "Redirected");
+                        //Debug.Assert(false, "Redirected");
                         break;
                 }
             }
@@ -186,8 +185,8 @@ namespace SpiderEngine
             }
             return true;
         }
-        private static List<string> supportedSchemes = new List<string> { "http", "https" };
-        private static Dictionary<string, string> tags2Attribute = new Dictionary<string, string>
+        private static readonly List<string> supportedSchemes = new List<string> { "http", "https" };
+        private static readonly Dictionary<string, string> tags2Attribute = new Dictionary<string, string>
         {
             { "a","href" },
             { "script","src" },
@@ -225,7 +224,7 @@ namespace SpiderEngine
                 string attributeName = pair.Value;
                 IEnumerable<HtmlNode> links = documentNode.Descendants(tagName);
                 List<Task> tasks = links
-                    .Select(link => ScanLinkAsync(steps, uri, attributeName, link, cancellationToken) )
+                    .Select(link => ScanLinkAsync(steps, uri, attributeName, link, cancellationToken))
                     .ToList();
                 await Task.WhenAll(tasks);
             }
@@ -263,7 +262,7 @@ namespace SpiderEngine
                         if (!isStillInSite)
                             return;
                     }
-                    await Process(steps, uri, derivedUri, mayContainLink, processChildrenLinks: true,cancellationToken);
+                    await Process(steps, uri, derivedUri, mayContainLink, processChildrenLinks: true, cancellationToken);
                 }
             }
         }
